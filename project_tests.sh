ce89896784b097da633691c0e5baa8c0a2b368a7
Luke <lsh-0@users.noreply.github.com>
2020-02-11 14:42:05 +1030

    updates references to python3.5 to python3 in general

--- project_tests.sh
+++ project_tests.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -e "venv/bin/python3.5" ]; then
-    echo "could not find venv/bin/python3.5, recreating venv"
-    rm -rf venv
-    virtualenv --python=python3.5 venv
-fi
+. mkvenv.sh
 
 source venv/bin/activate
 
