71d46f9c728dabc7c93da65583f64b3300753417
LSH <lsh-0@users.noreply.github.com>
2021-12-14 14:25:52 +1030

    removes coveralls (#54)
    * removes coveralls
    * adds install.sh, adds 'coverage' dependency, updates Jenkinsfile

--- project_tests.sh
+++ project_tests.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 set -e
-
-. mkvenv.sh
-
-source venv/bin/activate
-
-pip install -r requirements.txt
-pip install -r requirements-dev.txt
-
+. install.sh
 coverage run -m pytest --junitxml=build/pytest.xml
-
-COVERALLS_REPO_TOKEN=$(cat /etc/coveralls/tokens/elife-api-validator) coveralls
